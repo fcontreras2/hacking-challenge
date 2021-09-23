d01979f5a3a87a13abea5445131ce14a6a8b8bf2
Freddy Contreras <fcontreras@solera.pe>
2021-09-22 19:51:05 -0500

    - Integrando valores de configuración

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,3 +1,13 @@
-const Home = () => <p>Home</p>;
+import { Fragment } from "react";
+import {Helmet} from "react-helmet";
+
+const Home = () => 
+<Fragment>
+  <Helmet>
+   <title>Rimac - Seguro vehicular Tracking</title>
+
+    </Helmet>
+<p>Home</p>
+</Fragment>
 
 export default Home;
