6c435a6c01da70c565b61556a8a2deeb1a24d67f
Freddy Contreras <fcontreras@solera.pe>
2021-09-29 18:51:52 -0500

    - Sección de Summary

--- src/pages/LoadData/Summary/index.tsx
+++ src/pages/LoadData/Summary/index.tsx
@@ -3,13 +3,18 @@ import "./styles.scss";
 
 export const Summary = () => {
   return (
-    <div className="summary d-desktop">
-      <p className="summary__amount">MONTO</p>
-      <h6 className="summary__price">$35.00</h6>
-      <span className="summary__mouths">mensuales</span>
-      <div className="summary__divider"></div>
-      <p className="summary__include mb-1">El precio incluye:</p>
-      <ul className="summary__list mb-4">
+    <div className="summary">
+      <div className="summary__result d-mobile">
+        <h6 className="summary__price">$35.00</h6>
+        <span className="summary__mouths">MENSUAL</span>
+      </div>
+      
+      <p className="summary__amount d-desktop">MONTO</p>
+      <h6 className="summary__price d-desktop">$35.00</h6>
+      <span className="summary__mouths d-desktop">mensuales</span>
+      <div className="summary__divider d-desktop"></div>
+      <p className="summary__include mb-1 d-desktop">El precio incluye:</p>
+      <ul className="summary__list mb-4 d-desktop">
         <li className="mb-1"><img className="mr-2" src="/images/icon-check.svg" alt="Icono check" />Llanta de respuesto</li>
         <li className="mb-1"><img className="mr-2" src="/images/icon-check.svg" alt="Icono check" />Análisis de motor</li>
         <li className="mb-1"><img className="mr-2" src="/images/icon-check.svg" alt="Icono check" />Aros gratis</li>
